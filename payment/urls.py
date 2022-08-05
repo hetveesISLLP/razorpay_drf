c58ff11b6347a2b2acefee6a1c6eaade109ef4fc
@hetvees.inexture <hetvees.inexture@gmail.com>
2022-08-05 18:07:18 +0530

    razorpay with drf

--- payment/urls.py
+++ payment/urls.py
@@ -3,7 +3,7 @@ from .views import create_payment_link, PaymentHandler
 
 urlpatterns = [
     # for creating payment link
-    path('', create_payment_link, name='create_payment'),
+    path('',create_payment_link, name='create_payment'),
     # for verifying signature and handle payment
     path('callback-url/', PaymentHandler.as_view(), name='verify_pl')
 
