0a2baba4925c84f4d4c5fb86fd3976b119e33395
@hetvees.inexture <hetvees.inexture@gmail.com>
2022-08-16 17:45:21 +0530

    ENV

--- payment_razorpay/settings.py
+++ payment_razorpay/settings.py
@@ -28,7 +28,8 @@ RAZORPAY_SECRET_KEY = os.environ.get('RAZORPAY_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://payment-razorpay-drf.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['payment-razorpay-drf.herokuapp.com', '127.0.0.1']
+
 
 
 # Application definition
